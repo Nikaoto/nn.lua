eefdd5fbac531cfc56a0d1fb8dfd75f7b9df39b9
Nika Otiashvili <nikaoto@gmail.com>
2022-09-23 01:16:59 +0400

    add backprop, train and act functions

--- nn.lua
+++ nn.lua
@@ -5,6 +5,12 @@ local nn = {
    _LICENSE = [[ BSD3 ]]
 }
 
+-- For debugging
+local k=false
+local function printk(msg)
+   if k then print(msg) end
+end
+
 -- Defaults
 local def = {
    act_fns = {},
@@ -17,11 +23,7 @@ local def = {
    bias_min = -5,
    bias_max = 5,
    shuffle_training_data = false,
-   randomize_weights = true,
-   randomize_biases = true,
 }
-
-local fmt = string.format
 
 local function sq(x) return x*x end
 
@@ -37,6 +39,13 @@ local function map(arr, fn)
         new_arr[i] = fn(arr[i], i)
     end
     return new_arr
+end
+
+local function shuffle_array(arr)
+   for i=#arr, 1, -1 do
+      local j = math.random(1, i)
+      arr[j], arr[i] = arr[i], arr[j]
+   end
 end
 
 -- Make an array of 'nmemb' elements and set all of them to 'value'
@@ -65,9 +74,19 @@ local function calc_total_err(actual_arr, desired_arr)
    return loss
 end
 
+-- Sigmoid activation function and its derivative
+function nn.sigmoid(a) return 1 / (1 + math.exp(-a)) end
+-- 'sa' here is assumed to already be the sigmoid of the neuron activation
+function nn.d_sigmoid(sa) return sa * (1 - sa) end
+
+-- ReLU activation function and its derivative
+function nn.relu(a) return a > 0 and a or 0 end
+function nn.d_relu(a) return a > 0 and 1 or 0 end
+
 -- Returns a table that represents a neural network
 function nn.new_neural_net(opts)
    if not opts then opts = {} end
+
    local neuron_counts = opts.neuron_counts
    local act_fns       = opts.act_fns    or def.act_fns
    local d_act_fns     = opts.d_act_fns  or def.d_act_fns
@@ -141,14 +160,6 @@ function nn.new_neural_net(opts)
    return net
 end
 
-function nn.train(net, opts)
-   if not opts then opts = {} end
-   -- TODO: read opts
-   -- TODO: training loop, accumulate nudges using backprop and
-   --       print status
-   -- TODO: apply nudges
-end
-
 function nn.insert_inputs(net, inputs)
    for i, _ in ipairs(net.neurons[1]) do
       net.neurons[1][i] = inputs[i]
@@ -174,11 +185,11 @@ function nn.feedforward(net, opts)
          end
 
          -- Add bias
-         local act = sum + net.biases[li][ni]
+         local act = sum -- TODO: + net.biases[li][ni]
 
          -- Apply activation function
-         if net.act_fns and net.act_fns[li] then
-            net.neurons[li][ni] = net.act_fns[li](act)
+         if net.act_fns and net.act_fns[li-1] then
+            net.neurons[li][ni] = net.act_fns[li-1](act)
          else
             net.neurons[li][ni] = act
          end
@@ -188,5 +199,113 @@ function nn.feedforward(net, opts)
    return net.neurons[#net.neurons]
 end
 
+-- Expects 'training_data' to be shaped like:
+-- {
+--    { inputs = {...}, outputs = {...} },
+--    { inputs = {...}, outputs = {...} },
+--    ...
+-- }
+function nn.train(net, training_data, opts)
+   if not opts then opts = {} end
+
+   local learning_rate   = opts.learning_rate or def.learning_rate
+   local epochs          = opts.epochs        or def.epochs
+   local shuf            = opts.shuf          or def.shuffle_training_data
+   local log_freq        = opts.log_freq      or def.training_log_freq
+   local log_every       = 1 / log_freq
+
+   for iter=1, epochs do
+      if shuf then shuffle_array(training_data) end
+
+      local avg_loss = 0
+      for _, data in ipairs(training_data) do
+         local out = nn.feedforward(net, { inputs = data.inputs })
+
+         -- Update average loss
+         local loss = calc_total_err(out, data.outputs)
+         avg_loss = avg_loss + loss / #training_data
+
+         -- Do backpropagation
+         local nudges = nn.backprop(net, data.outputs, learning_rate)
+         nn.apply_nudges(net, nudges)
+      end
+
+      -- Log status
+      if iter % log_every == 0 then
+         print(("epoch = %i, avg_loss = %g"):format(iter, avg_loss))
+      end
+   end
+end
+
+-- Does backpropagation for a single training example.
+-- Returns a list of nudges for the weights and the biases.
+function nn.backprop(net, out, rate)
+   local wnudges = {}
+   local bnudges = {}
+
+   local curr_der -- for using the stored derivatives
+   local next_der -- for storing derivatives for propagation
+
+   -- Calc pd_error / pd_output
+   curr_der = map(net.neurons[#net.neurons], function(o, i)
+       return calc_d_err(o, out[i])
+   end)
+   next_der = {}
+
+   -- Iterate backwards from output layer
+   for li=#net.weights, 1, -1 do
+      wnudges[li] = {}
+      bnudges[li] = {}
+      for wi, _ in ipairs(net.weights[li]) do
+         -- Neuron at index 'ni1' feeds into the neuron at index 'ni2'
+         -- using the current weight 'w'.
+         local ni1 = 1 + math.floor((wi-1) / #net.neurons[li+1])
+         local ni2 = 1 + (wi-1) % #net.neurons[li+1]
+
+         -- pd_activation / pd_neuron
+         local a = net.d_act_fns[li] and
+                   net.d_act_fns[li](net.neurons[li+1][ni2]) or 1
+
+         -- Final nudge for this weight
+         wnudges[li][wi] = -1 * rate * curr_der[ni2] * net.neurons[li][ni1] * a
+
+         printk(("ni1=%i, ni2=%i, li=%i, wi=%i, e=%g, a=%g, grad=%g")
+               :format(ni1, ni2, li, wi, curr_der[ni2], a, wnudges[li][wi]))
+
+         -- Propagate derivatives to the layers that follow
+         next_der[ni1] = (next_der[ni1] or 0) +
+                         curr_der[ni2] * a * net.weights[li][wi]
+
+         -- TODO: bnudges
+      end
+
+      -- Shift the derivative buffers
+      curr_der = next_der
+      next_der = {}
+   end
+
+   return {
+      weights = wnudges,
+      biases = bnudges,
+   }
+end
+
+function nn.apply_nudges(net, nudges)
+   local wnudges = nudges.weights
+   -- TODO: local bnudges = nudges.biases
+
+   for li, _ in ipairs(net.weights) do
+      for wi, _ in ipairs(net.weights[li]) do
+         net.weights[li][wi] = net.weights[li][wi] + wnudges[li][wi]
+      end
+   end
+
+   -- TODO:
+   -- for li, _ in ipairs(net.biases) do
+   --    for bi, _ in ipairs(net.biases[li]) do
+   --       net.biases[li][bi] = net.biases[li][bi] + bnudges[li][bi]
+   --    end
+   -- end
+end
 
 return nn
--- tmp.lua
+++ tmp.lua
@@ -1,27 +1,46 @@
+inspect = require("inspect")
 local nn = require("nn")
---local training_data, testing_data = require("data")
 
+-- Training data (XOR function)
+local training_data = {
+   { inputs={1,1}, outputs={0} },
+   { inputs={1,0}, outputs={1} },
+   { inputs={0,1}, outputs={1} },
+   { inputs={0,0}, outputs={0} }
+}
+local testing_data = training_data
+
+-------------
+print("\nNet")
 math.randomseed(6929)
-
 local net = nn.new_neural_net({
-   neuron_counts = {1, 4, 2},
-   act_fns = {nil, sigmoid, sigmoid},
-   d_act_fns = {nil, d_sigmoid, d_sigmoid},
+   neuron_counts = {2, 3, 1},
+   act_fns = {nn.sigmoid, nn.sigmoid},
+   d_act_fns = {nn.d_sigmoid, nn.d_sigmoid},
+   biases = {
+      {2.8, 2.8},
+      {-4.31,-4.31,-4.31},
+      {-0.68},
+   }
 })
+print(inspect(net))
 
+------------------
+print("\nTraining")
 nn.train(net, training_data, {
-   epochs = 1000,
+   epochs = 2500,
    learning_rate = 0.1,
-   log_freq = 0.5,
+   log_freq = 0.01,
 })
 
+------------------
+print("\nTesting")
 for i, data in ipairs(testing_data) do
    local out = nn.feedforward(net, {
       inputs = data.inputs
    })
    print(string.format([[
-test %i
-data: %s
-prediction: %s
------]], i, inspect(data), inspect(out)))
+-----
+test %i: %s
+prediction: %s]], i, inspect(data), inspect(out)))
 end
