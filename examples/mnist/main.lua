979f799b3641685835d27e309215d38f7346fa76
Nika Otiashvili <nikaoto@gmail.com>
2022-10-17 00:17:50 +0400

    add indexing; read labels; go back with backspc

--- examples/mnist/main.lua
+++ examples/mnist/main.lua
@@ -1,4 +1,5 @@
 local inspect = require("inspect")
+local lg = love.graphics
 
 --[[
 Image file format
@@ -16,11 +17,24 @@ xxxx     unsigned byte   ??               pixel
 
 ]]--
 
+local IMAGE_COUNT = 60000
 local SZ = 10
 local ROWS, COLS = 28, 28
 
-local file = io.open("train-images-idx3-ubyte", "r")
-file:seek("set", 14)
+local IMAGES_FILE_LOC_START = 16
+local LABELS_FILE_LOC_START = 8
+local images_file_loc = IMAGES_FILE_LOC_START
+local labels_file_loc = LABELS_FILE_LOC_START
+local label_x, label_y = 30, 30
+local ind_x, ind_y = label_x, label_y + 20
+local image_x, image_y = label_x, ind_y + 50
+local image_ind = 0
+
+local images_file = io.open("train-images-idx3-ubyte", "r")
+images_file:seek("set", images_file_loc)
+
+local labels_file = io.open("train-labels-idx1-ubyte", "r")
+labels_file:seek("set", labels_file_loc)
 
 function eat(file, nbytes)
    local str = file:read(nbytes)
@@ -28,11 +42,19 @@ function eat(file, nbytes)
 end
 
 local img
+local label
 
 function next_img()
+   image_ind = image_ind + 1
+
+   -- Read label
+   local lbl = eat(labels_file, 1)
+   label = lbl[1] or -1
+
+   -- Read image
    img = {}
    for i=1, ROWS do
-      table.insert(img, eat(file, COLS))
+      table.insert(img, eat(images_file, COLS))
    end
 end
 
@@ -41,12 +63,24 @@ function love.load()
 end
 
 function love.draw()
+   lg.setColor(1, 1, 1, 1)
+   -- Draw index
+   lg.print(("image_ind = %d/%d"):format(image_ind, IMAGE_COUNT), ind_x, ind_y, 0, 2, 2)
+   -- Draw label
+   lg.print(("label = %d"):format(label), label_x, label_y, 0, 2, 2)
+   
+   -- Draw image outline
+   lg.setColor(0, 1, 1, 1)
+   lg.rectangle("line", image_x, image_y, SZ*COLS, SZ*ROWS)
+
+   -- Draw image
    for col, _ in ipairs(img) do
       for row, _ in ipairs(img[col]) do
-         love.graphics.setColor(1, 1, 1, img[row][col]/255)
-         love.graphics.rectangle(
+         lg.setColor(1, 1, 1, img[row][col]/255)
+         lg.rectangle(
             "fill",
-            col*SZ, row*SZ,
+            image_x + col*SZ,
+            image_y + row*SZ,
             SZ, SZ)
       end
    end
@@ -54,6 +88,22 @@ end
 
 function love.keypressed(key)
    if key == "space" then
+      next_img()
+   elseif key == "backspace" then
+      image_ind = image_ind - 2
+      labels_file_loc = labels_file:seek("cur") - 2
+      images_file_loc = images_file:seek("cur") - COLS * ROWS * 2
+
+      if labels_file_loc < LABELS_FILE_LOC_START then
+         labels_file_loc = LABELS_FILE_LOC_START
+      end
+      if images_file_loc < IMAGES_FILE_LOC_START then
+         images_file_loc = IMAGES_FILE_LOC_START
+      end
+      if image_ind < 0 then image_ind = 0 end
+
+      labels_file:seek("set", labels_file_loc)
+      images_file:seek("set", images_file_loc)
       next_img()
    end
 end
